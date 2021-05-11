97c27a19cd7001171e5df42f11f7751005580687
svhenrique <silviohenrique1234@hotmail.com>
2021-05-11 17:49:50 -0300

    login adicionado na url de core

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path, include
-from .views import CadastroView
+from .views import CadastroView, LogarView
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('cadastro/', CadastroView.as_view(), name="cadastro"),
+    path('login/', LogarView.as_view(), name="login"),
 ]
