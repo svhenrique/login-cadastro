6fe7503bbfdf67cc56449647fc874102cf6c168f
svhenrique <silviohenrique1234@hotmail.com>
2021-05-12 16:51:23 -0300

    adiciondo frontend login e cadastro no app

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
+    path('conta/login/', LogarView.as_view(), name="login"), # deve-se colocar primeiro pois fica com prioridade e nao usa as rotas de mesmo nome do include abaixo
     path('conta/', include('django.contrib.auth.urls')),  # rotas de atuenticação do próprio django,
     path('conta/cadastro/', CadastroView.as_view(), name="cadastro"),
-    path('conta/login/', LogarView.as_view(), name="login"),
 ]
