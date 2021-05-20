0c0ebf28b177e82beff8a39cba53dbd2c8bc00e9
svhenrique <silviohenrique1234@hotmail.com>
2021-05-20 11:33:00 -0300

    consertado erro de tamanho max_size

--- core/validators.py
+++ core/validators.py
@@ -3,10 +3,14 @@ from django.core.exceptions import ValidationError
 
 allowed_formats = "'.gif', '.jpg', '.jpeg', '.png'"
 
-max_size = 4*10000000  # byte format
+max_size = 5*1000000  # byte format
+
+def byte_to_mb(byte_file):
+    to_kB = to_MB = 1024
+    return byte_file//(to_kB*to_MB)
 
 mensagens_erros = {
-    'max_size': f"A imagem não pode ser maior que {max_size} kB.",
+    'max_size': f"A imagem não pode ser maior que {byte_to_mb(max_size)} MB.",
     'content_type': f"Apenas arquivos no formato {allowed_formats} são suportados"
 }
 
@@ -18,6 +22,7 @@ def validate_file_format(archive):
 
 def validate_file_size(archive):
     file_size = archive.size
+    print(file_size)
     if file_size > max_size:
         raise ValidationError(mensagens_erros['max_size'])
     return archive
