71243d2c62a715be6bf7dbbca83cc8f81217cdfd
svhenrique <silviohenrique1234@hotmail.com>
2021-07-07 13:28:50 -0300

    rotas de password adiciondas

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,18 @@
 from django.urls import path, include
 from .views import CadastroView, LogarView, IndexView
 from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
-    path('conta/login/', LogarView.as_view(), name="login"), # deve-se colocar primeiro pois fica com prioridade e nao usa as rotas de mesmo nome do include abaixo
+
     path('conta/cadastro/', CadastroView.as_view(), name="cadastro"),
-    path('conta/', include('django.contrib.auth.urls')),  # rotas de atuenticação do próprio django,
+
+    path('conta/login/', LogarView.as_view(), name="login"),
+    path('conta/logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('conta/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('conta/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('conta/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('conta/reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
