a95c043ed6aa34be688355249574c08213613a56
svhenrique <silviohenrique1234@hotmail.com>
2021-07-07 14:05:01 -0300

    adicionado form_class de CustomPasswordResetForm

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path, include
 from .views import CadastroView, LogarView, IndexView
+from .forms import CustomPasswordResetForm
 from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth_views
 
@@ -12,7 +13,7 @@ urlpatterns = [
     path('conta/login/', LogarView.as_view(), name="login"),
     path('conta/logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    path('conta/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('conta/password_reset/', auth_views.PasswordResetView.as_view(form_class=CustomPasswordResetForm), name='password_reset'),
     path('conta/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('conta/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('conta/reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
