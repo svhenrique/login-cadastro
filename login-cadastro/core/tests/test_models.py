0cc03de17a24aca4c30348da03e74df940331779
svhenrique <silviohenrique1234@hotmail.com>
2021-07-15 16:14:20 -0300

    adicionado UsuarioManagerTestCase

--- login-cadastro/core/tests/test_models.py
+++ login-cadastro/core/tests/test_models.py
@@ -26,3 +26,37 @@ class CustomUsuarioTestCase(TestCase):
     def test_image_url(self):
         self.assertEquals("/static/img/blank-profile.png", self.user.get_image())
 
+class UsuarioManagerTestCase(TestCase):
+
+    def setUp(self):
+        self.user = mommy.make('CustomUsuario')
+        self.data = {
+            'first_name': 'Teste',
+            'last_name': 'dos Santos',
+            'username': 'teste@gmail.com',
+            'password': 'tt_@_3_@_2_4',
+            'image': 'teste.png'
+        }
+
+    def test_user_is_staff(self):
+        self.assertEquals(False, self.user.is_staff)
+
+    def test_user_is_superuser(self):
+        self.assertEquals(False, self.user.is_superuser)
+
+    def test_user_creation(self):
+
+        username = self.data['username']
+        password = self.data['password']
+        first_name = self.data['first_name']
+        last_name = self.data['last_name']
+        password = self.data['password']
+        image = self.data['image']
+
+        user = CustomUsuario.objects.create_user(username, password, first_name=first_name, image=image, last_name=last_name)
+        user_in_db = CustomUsuario.objects.get(id=user.id)
+
+        self.assertEquals(user, user_in_db)
+
+
+
