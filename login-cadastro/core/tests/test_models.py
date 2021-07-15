bc3acb02f1e9d0170933352b3cb5428e4067d51e
svhenrique <silviohenrique1234@hotmail.com>
2021-07-15 11:49:16 -0300

    adicionado teste para get_file_path

---
+++ login-cadastro/core/tests/test_models.py
@@ -0,0 +1,14 @@
+import uuid
+from django.test import TestCase
+
+from core.models import get_file_path
+
+
+class GetFilePathTestCase(TestCase):
+
+    def setUp(self):
+        self.filename = f'{uuid.uuid4()}.png'
+
+    def test_get_file_path(self):
+        arquivo = get_file_path(None, 'teste.png')
+        self.assertTrue(len(arquivo), len(self.filename))
