3ac7e430607f4f0be29a95576253f91bc2e167cc
svhenrique <silviohenrique1234@hotmail.com>
2021-07-27 09:35:30 -0300

    preparado teste case para CustomUsuarioCreateForm

--- login-cadastro/core/tests/test_forms.py
+++ login-cadastro/core/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-
-from core.forms import CustomPasswordResetForm
+from model_mommy import mommy
+from core.forms import CustomPasswordResetForm, CustomUsuarioCreateForm
 from core.email_sender import EmailMessage
 from django.template import loader
 
@@ -44,3 +44,23 @@ class CustomPasswordResetFormTestCase(TestCase):
     def test_form_valid(self):
         form = self.form
         self.assertEquals(form.is_valid(), True)
+
+class CustomUsuarioCreateFormTestCase(TestCase):
+
+    def setUp(self):
+        self.first_name = 'Felicity',
+        self.last_name = 'Jones',
+        self.username = 'felicity@gmail.com'
+        self.password = "cc_kk_ll_nn2"
+        self.data = {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "username": self.username,
+            "password1": self.password,
+            "password2": self.password
+        }
+        self.create_form = CustomUsuarioCreateForm(data=self.data)
+
+
+
+
