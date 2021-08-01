8fe069fd1e926edb7485a83963f7990b49d38f76
svhenrique <silviohenrique1234@hotmail.com>
2021-08-01 01:33:16 -0300

    classe CadastroViwTestCase criada

--- login-cadastro/core/tests/test_views.py
+++ login-cadastro/core/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.test import Client
 from django.urls import reverse_lazy
 from core.forms import CustomUsuarioCreateForm
+from core.models import CustomUsuario
 
 class IndexViewTestCase(TestCase):
 
@@ -32,3 +33,16 @@ class IndexViewTestCase(TestCase):
         request = self.client.get(reverse_lazy('index'), follow=True)
         redirects = len(request.redirect_chain)
         self.assertEquals(redirects, 0)
+
+class CadastroViewTestCase(TestCase):
+
+    def setUp(self):
+        self.data = {
+            'first_name': 'Felicity',
+            'last_name': 'Jones',
+            'username': 'felicity@gmail.com',
+            'password1': 'cc_kk_ll_nn2',
+            'password2': 'cc_kk_ll_nn2'
+        }
+        self.user_creation_form = CustomUsuarioCreateForm(data=self.data)
+        self.client = Client()
