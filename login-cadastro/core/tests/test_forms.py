739ac22a231e3e51f00d0410d9e7146e83aa0216
svhenrique <silviohenrique1234@hotmail.com>
2021-08-03 16:09:04 -0300

    test_forms refatorado

--- login-cadastro/core/tests/test_forms.py
+++ login-cadastro/core/tests/test_forms.py
@@ -1,19 +1,13 @@
 from django.test import TestCase
-from model_mommy import mommy
 from core.forms import CustomPasswordResetForm, CustomUsuarioCreateForm
-from core.email_sender import EmailMessage
-from django.template import loader
-from decouple import config
-
-EMAIL_TEST = config('EMAIL_TEST')
 
 class CustomPasswordResetFormTestCase(TestCase):
 
     def setUp(self):
-        self.first_name = 'Felicity',
-        self.last_name = 'Jones',
-        self.username = EMAIL_TEST
-        self.password = "cc_kk_ll_nn2"
+        self.first_name = 'Felicity'
+        self.last_name = 'Jones'
+        self.username = 'felicity@gmail.com'
+        self.password = 'cc_kk_ll_nn2'
         self.data = {
             "first_name": self.first_name,
             "last_name": self.last_name,
@@ -23,9 +17,6 @@ class CustomPasswordResetFormTestCase(TestCase):
         }
         self.form = CustomPasswordResetForm(data={"email": self.username})
 
-    def test_send_mail_status_code(self):
-        response = self.form.send_mail(data=self.data)
-        self.assertEquals(response, 202)
 
     def test_form_valid(self):
         form = self.form
