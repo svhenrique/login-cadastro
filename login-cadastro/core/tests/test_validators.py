15086d0c26afa373b6ad754e220cbc8c681a3ba9
svhenrique <silviohenrique1234@hotmail.com>
2021-08-04 09:56:15 -0300

    adicionado classe de teste ValidatorsTestCase

--- login-cadastro/core/tests/test_validators.py
+++ login-cadastro/core/tests/test_validators.py
@@ -11,4 +11,10 @@ class File:
         self.name = name
         self.size = size
 
+class ValidatorsTestCase(TestCase):
+
+    def setUp(self):
+        self.allowed_images = [File('test.png', 11), File('test.jpg', 20), File('test.jpeg', 10), File('test.gif', 15)]
+        self.not_allowed_images = [File('test.zip', 20), File('test.raw', 10), File('test.bmp', 12)]
+        self.big_image = File('test.png', 100000000000000)
 
