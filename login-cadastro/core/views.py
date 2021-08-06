d87f0c60d2b3318a571cfc91b87ca308bbe7d86f
svhenrique <silviohenrique1234@hotmail.com>
2021-08-06 09:09:57 -0300

    adicionado função _make_confirmation_message para CadastroView

--- login-cadastro/core/views.py
+++ login-cadastro/core/views.py
@@ -39,6 +39,19 @@ class CadastroView(FormView):
         user.save()
         return user
 
+    # ver o que render_to_string faz
+    def _make_confirmation_message(self, user, *args, **kwargs):
+        current_site = get_current_site(self.request)
+        mail_subject = 'Activate your account.'
+        message = render_to_string('acc_active_email.html', {
+            'user': user,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': account_activation_token.make_token(user),
+        })
+        to_email = user.email
+        return to_email, mail_subject, message
+
     def get(self, request, *args, **kwargs):
         if request.user.is_anonymous:
             return super(CadastroView, self).get(request, *args, **kwargs)
