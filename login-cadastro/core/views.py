91309999a9cc9432e3f02fbdf5e0ba9012eea182
svhenrique <silviohenrique1234@hotmail.com>
2022-01-17 23:39:24 -0300

    adicionado classe ActivateView

--- login-cadastro/core/views.py
+++ login-cadastro/core/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from .forms import CustomUsuarioCreateForm, CustomLoginForm
@@ -9,6 +10,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_text
 from .tokens import account_activation_token
 from .email_sender import EmailMessage
+from django.contrib.auth import get_user_model
 
 """
 Criar class based view para página de registro e deixar página vinculada ao admin
@@ -16,6 +18,8 @@ quieto
 
 link para ajuda: https://www.youtube.com/watch?v=Ev5xgwndmfc&feature=emb_title
 """
+
+UserModel = get_user_model()
 
 class IndexView(TemplateView):
 
@@ -43,7 +47,7 @@ class CadastroView(FormView):
     def _make_confirmation_message(self, user, *args, **kwargs):
         current_site = get_current_site(self.request)
         mail_subject = 'Activate your account.'
-        message = render_to_string('acc_active_email.html', {
+        message = render_to_string('acc_activate_email.html', {
             'user': user,
             'domain': current_site.domain,
             'uid': urlsafe_base64_encode(force_bytes(user.pk)),
@@ -68,11 +72,37 @@ class CadastroView(FormView):
         # e conectar usuario do cadastro
         user = self._user_form(form)
         email_message = self._make_confirmation_message(user)
-        self._send_confirmation(email_message)
+        self._send_confirmation(*email_message)
         return super(CadastroView, self).form_valid(form)
 
     def form_invalid(self, form, *args, **kwargs):
         return super(CadastroView, self).form_invalid(form)
+
+class ActivateView(TemplateView):
+
+    def get_user(self, uidb64):
+        try:
+            # urlsafe_base64_decode() decodes to bytestring
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = UserModel._default_manager.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, UserModel.DoesNotExist, ValidationError):
+            user = None
+        return user
+
+    def dispatch(self, request, *args, **kwargs):
+
+        assert 'uidb64' in kwargs and 'token' in kwargs
+
+        user = self.get_user(kwargs['uidb64'])
+        token = kwargs['token']
+
+        if user is not None and account_activation_token.check_token(user, token):
+            user.is_active = True
+            user.save()
+            return redirect('success')
+        else:
+            user.delete()
+            return redirect('failure')
 
 class LogarView(LoginView):
 
@@ -81,6 +111,7 @@ class LogarView(LoginView):
 
     # nao e necessario por o success_url pois ja existe uma rota de redirecionamento
     # chamada LOGIN_REDIRECT_URL em settings.py
+
 
 class SucessoView(TemplateView):
 
